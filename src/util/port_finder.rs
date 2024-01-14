2ee00bc57516d5c626578752794e1ed60a240ebc
Yorick De Jong <yorickdejong@gmail.com>
2024-01-14 18:47:14 +0100

    Auto stash before rebase of "origin/main"

--- src/util/port_finder.rs
+++ src/util/port_finder.rs
@@ -9,51 +9,51 @@ pub struct PortGenerator {
 }
 
 impl PortGenerator {
-    /// Creates a new PortGenerator with the specified port range.
-    ///
-    /// # Arguments
-    /// * `start_range` - The starting port number of the range.
-    /// * `end_range` - The ending port number of the range.
-    pub fn new(start_range: u16, end_range: u16) -> Self {
-        PortGenerator {
-            port_range: format!("{}-{}", start_range, end_range),
-            selected_port: None,
-        }
+/// Creates a new PortGenerator with the specified port range.
+///
+/// # Arguments
+/// * `start_range` - The starting port number of the range.
+/// * `end_range` - The ending port number of the range.
+pub fn new(start_range: u16, end_range: u16) -> Self {
+    PortGenerator {
+        port_range: format!("{}-{}", start_range, end_range),
+        selected_port: None,
     }
+}
 
-    /// Attempts to find a free port within the specified range.
-    ///
-    /// # Returns
-    /// `Some(port)` if a free port is found, or `None` if no free port is available.
-    //TODO: Maybe we need to return the random number as a Result and implement proper error handeling
-    pub fn generate(&mut self) -> &mut Self {
-        let mut rng = thread_rng();
-        let port_range: Vec<u16> = (self.port_range_start()..=self.port_range_end()).collect();
-        let mut shuffled_ports = port_range;
-        shuffled_ports.shuffle(&mut rng);
+/// Attempts to find a free port within the specified range.
+///
+/// # Returns
+/// `Some(port)` if a free port is found, or `None` if no free port is available.
+//TODO: Maybe we need to return the random number as a Result and implement proper error handeling
+pub fn generate(&mut self) -> &mut Self {
+	let mut rng = thread_rng();
+	let port_range: Vec<u16> = (self.port_range_start()..=self.port_range_end()).collect();
+	let mut shuffled_ports = port_range;
+	shuffled_ports.shuffle(&mut rng);
 
-        for port in shuffled_ports {
-            if TcpListener::bind(("127.0.0.1", port)).is_ok() {
-                self.selected_port = Some(port);
-                break; // Exit the loop once a port is found
-            }
-        }
+	for port in shuffled_ports {
+		if TcpListener::bind(("127.0.0.1", port)).is_ok() {
+			self.selected_port = Some(port);
+			break; // Exit the loop once a port is found
+		}
+	}
 
-        self // Return a mutable reference to the object itself
-    }
+	self // Return a mutable reference to the object itself
+}
 
-    /// Gets the start of the port range.
-    fn port_range_start(&self) -> u16 {
-        self.port_range.split('-').next().unwrap().parse().unwrap()
-    }
+/// Gets the start of the port range.
+fn port_range_start(&self) -> u16 {
+	self.port_range.split('-').next().unwrap().parse().unwrap()
+}
 
-    /// Gets the end of the port range.
-    fn port_range_end(&self) -> u16 {
-        self.port_range.split('-').last().unwrap().parse().unwrap()
-    }
+/// Gets the end of the port range.
+fn port_range_end(&self) -> u16 {
+	self.port_range.split('-').last().unwrap().parse().unwrap()
+}
 
-    /// Returns the selected port, if available.
-    pub fn selected_port(&self) -> Option<u16> {
-        self.selected_port
-    }
+/// Returns the selected port, if available.
+pub fn selected_port(&self) -> Option<u16> {
+	self.selected_port
+}
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -64,81 +64,87 @@ async fn insert_and_find_multiple_instances() {
 #[cfg_attr(feature = "tokio-runtime", tokio::test)]
 #[cfg_attr(feature = "async-std-runtime", async_std::test)]
 async fn insert_and_find_multiple_instances_2() {
-    let instance_count = 5;
+let instance_count = 5;
 
-    let handles = (0..instance_count)
-        .map(|_| {
-            tokio::spawn(async move {
-                let_assert!(Ok(mongo) = TempMongo::new().await);
-                let database = mongo.client().database("test_2");
-                let collection = database.collection::<Document>("foo");
+let handles = (0..instance_count)
+.map(|_| {
+	tokio::spawn(async move {
+		let_assert!(Ok(mongo) = TempMongo::new().await);
+		let database = mongo.client().database("test_2");
+		let collection = database.collection::<Document>("foo");
 
-                let_assert!(Ok(id) = collection.insert_one(doc! { "hello": "world" }, None).await);
-                let_assert!(Some(id) = id.inserted_id.as_object_id());
-                let_assert!(
-                    Ok(Some(document)) = collection.find_one(doc! { "_id": id }, None).await
-                );
-                assert_eq!(document, doc! { "_id": id, "hello": "world" });
-                assert!(let Ok(()) = mongo.kill_and_clean().await);
-            })
-        })
-        .collect::<Vec<_>>();
+		let_assert!(Ok(id) = collection.insert_one(doc! { "hello": "world" }, None).await);
+		let_assert!(Some(id) = id.inserted_id.as_object_id());
+		let_assert!(
+			Ok(Some(document)) = collection.find_one(doc! { "_id": id }, None).await
+		);
+		assert_eq!(document, doc! { "_id": id, "hello": "world" });
+		assert!(let Ok(()) = mongo.kill_and_clean().await);
+	})
+})
+.collect::<Vec<_>>();
 
-    // Await all handles and check for errors.
-    for handle in handles {
-        let result = handle.await;
-        assert!(result.is_ok());
-    }
+// Await all handles and check for errors.
+for handle in handles {
+let result = handle.await;
+assert!(result.is_ok());
+}
 }
 
+
+
+/// Seeds document into database, retrieves the seeded documents and 
+/// tests if the collected documents resemble the input documents
 #[cfg_attr(feature = "tokio-runtime", tokio::test)]
 #[cfg_attr(feature = "async-std-runtime", async_std::test)]
 async fn seeding_document() {
-    let documents = vec![
-        mongodb::bson::doc! {"name": "Alice", "age": 30},
-        mongodb::bson::doc! {"name": "Bob", "age": 25},
-    ];
+	let documents = vec![
+		mongodb::bson::doc! {"name": "Alice", "age": 30},
+		mongodb::bson::doc! {"name": "Bob", "age": 25},
+	];
 
-    // Create a new mongo instance and assert it's created successfully
-    let mongo = TempMongo::new()
-        .await
-        .expect("Failed to create TempMongo instance");
+	// Create a new mongo instance and assert it's created successfully
+	let mongo = TempMongo::new()
+		.await
+		.expect("Failed to create TempMongo instance");
 
-    // Prepare seed document
-    let prepared_seed_data = mongo.prepare_seed_document("test_3", "trex", documents.clone());
+	// Prepare seed document
+	let prepared_seed_data = mongo.prepare_seed_document("test_3", "trex", documents.clone());
 
-    match mongo.seed_document(&prepared_seed_data).await {
-        Ok(_) => println!("Data seeded successfully."),
-        Err(e) => println!("Error seeding data: {:?}", e),
-    }
+	match mongo.seed_document(&prepared_seed_data).await {
+		Ok(_) => println!("Data seeded successfully."),
+		Err(e) => println!("Error seeding data: {:?}", e),
+	}
 
-    // Fetch documents from the database and compare
-    let collection: mongodb::Collection<mongodb::bson::Document> =
-        mongo.client().database("test_3").collection("trex");
-    let mut cursor = collection
-        .find(None, None)
-        .await
-        .expect("Failed to execute find command");
+	// Fetch documents from the database and compare
+	let collection: mongodb::Collection<mongodb::bson::Document> =
+		mongo.client().database("test_3").collection("trex");
+	let mut cursor = collection
+		.find(None, None)
+		.await
+		.expect("Failed to execute find command");
 
-    // Collect documents from cursor
-    let mut fetched_documents = Vec::new();
-    while let Some(doc) = cursor
-        .try_next()
-        .await
-        .expect("Failed during cursor traversal")
-    {
-        fetched_documents.push(doc);
-    }
+	// Collect documents from cursor
+	let mut fetched_documents = Vec::new();
+	while let Some(doc) = cursor
+		.try_next()
+		.await
+		.expect("Failed during cursor traversal")
+	{
+		fetched_documents.push(doc);
+	}
 
-    // Remove '_id' field from the fetched documents
-    for doc in &mut fetched_documents {
-        doc.remove("_id");
-    }
+	// Remove '_id' field from the fetched documents
+	for doc in &mut fetched_documents {
+		doc.remove("_id");
+	}
 
-    // Assert that the fetched documents match what was seeded
-    assert_eq!(
-        documents, fetched_documents,
-        "The seeded documents do not match the fetched documents"
-    );
-    assert!(let Ok(()) = mongo.kill_and_clean().await);
+	// Assert that the fetched documents match what was seeded
+	assert_eq!(
+		documents, fetched_documents,
+		"The seeded documents do not match the fetched documents"
+	);
+
+	assert!(let Ok(()) = mongo.kill_and_clean().await);
+
 }
