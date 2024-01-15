f83388a08087ff46b6de8af8c34a342a7c2dcb24
Yorick De Jong <yorickdejong@gmail.com>
2024-01-15 11:18:43 +0100

    added tab indentation

--- src/error.rs
+++ src/error.rs
@@ -2,69 +2,69 @@ use std::path::PathBuf;
 
 /// An error that can occur when creating or cleaning a MongDB instance.
 pub struct Error {
-    /// The actual error.
-    inner: ErrorInner,
+	/// The actual error.
+	inner: ErrorInner,
 }
 
 #[derive(Debug)]
 pub enum ErrorInner {
-    /// Failed to create the temporary directory.
-    MakeTempDir(std::io::Error),
+	/// Failed to create the temporary directory.
+	MakeTempDir(std::io::Error),
 
-    /// Failed to create the database directory.
-    MakeDbDir(PathBuf, std::io::Error),
+	/// Failed to create the database directory.
+	MakeDbDir(PathBuf, std::io::Error),
 
-    /// Failed to spawn the server.
-    SpawnServer(String, std::io::Error),
+	/// Failed to spawn the server.
+	SpawnServer(String, std::io::Error),
 
-    /// Failed to kill the server.
-    KillServer(std::io::Error),
+	/// Failed to kill the server.
+	KillServer(std::io::Error),
 
-    /// Failed to clean up the temporary directory.
-    CleanDir(PathBuf, std::io::Error),
+	/// Failed to clean up the temporary directory.
+	CleanDir(PathBuf, std::io::Error),
 
-    /// Failed to connect to the server.
-    Connect(String, mongodb::error::Error),
+	/// Failed to connect to the server.
+	Connect(String, mongodb::error::Error),
 
-    NoFreePortFound,
+	Port,
 }
 
 impl std::error::Error for Error {}
 
 impl std::fmt::Debug for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        std::fmt::Debug::fmt(&self.inner, f)
-    }
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		std::fmt::Debug::fmt(&self.inner, f)
+	}
 }
 
 impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        std::fmt::Display::fmt(&self.inner, f)
-    }
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		std::fmt::Display::fmt(&self.inner, f)
+	}
 }
 
 impl std::fmt::Display for ErrorInner {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::MakeTempDir(e) => write!(f, "Failed to create temporary directory: {e}"),
-            Self::MakeDbDir(path, e) => {
-                write!(f, "Failed to create data directory {}: {e}", path.display())
-            }
-            Self::SpawnServer(name, e) => write!(f, "Failed to run server command: {name}: {e}"),
-            Self::KillServer(e) => write!(f, "Failed to terminate spanwed server: {e}"),
-            Self::CleanDir(path, e) => write!(
-                f,
-                "Failed to clean up temporary state directory {}: {e}",
-                path.display()
-            ),
-            Self::Connect(address, e) => write!(f, "Failed to connect to server at {address}: {e}"),
-            Self::NoFreePortFound => write!(f, "Failed to connect to a port "),
-        }
-    }
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Self::MakeTempDir(e) => write!(f, "Failed to create temporary directory: {e}"),
+			Self::MakeDbDir(path, e) => {
+				write!(f, "Failed to create data directory {}: {e}", path.display())
+			}
+			Self::SpawnServer(name, e) => write!(f, "Failed to run server command: {name}: {e}"),
+			Self::KillServer(e) => write!(f, "Failed to terminate spanwed server: {e}"),
+			Self::CleanDir(path, e) => write!(
+				f,
+				"Failed to clean up temporary state directory {}: {e}",
+				path.display()
+			),
+			Self::Connect(address, e) => write!(f, "Failed to connect to server at {address}: {e}"),
+			Self::Port => write!(f, "Failed to select a free port by the os "),
+		}
+	}
 }
 
 impl From<ErrorInner> for Error {
-    fn from(inner: ErrorInner) -> Self {
-        Self { inner }
-    }
+	fn from(inner: ErrorInner) -> Self {
+		Self { inner }
+	}
 }
--- src/util/port_finder.rs
+++ src/util/port_finder.rs
@@ -1,65 +1,42 @@
 use std::net::TcpListener;
 
-/// `PortGenerator` is a utility class for generating an available network port.
-///
-/// This class is particularly useful for scenarios where an application needs to
-/// bind to an available port without specifying a particular one. It leverages the
-/// operating system's ability to select an ephemeral port, ensuring that the chosen
-/// port is free at the time of selection.
-///
-/// The main use case of this class is in situations where a service or a server
-/// instance (like a database server in testing environments) needs to be started
-/// dynamically on a free port to avoid conflicts or for parallel testing.
-
+/// Represents a Port Generator to find available ports within a specified range.
 pub struct PortGenerator {
-    /// The port number selected by the operating system.
-    /// This is `None` until `generate()` successfully assigns a port.
-    selected_port: Option<u16>,
+	selected_port: Option<u16>,
 }
 
 impl PortGenerator {
-    /// Constructs a new `PortGenerator`.
-    ///
-    /// Initially, no port is selected (`selected_port` is `None`).
-    /// The `generate` method must be called to select a port.
-    ///
-    pub fn new() -> Self {
-        PortGenerator {
-            selected_port: None,
-        }
-    }
+	/// Constructs a new `PortGenerator`.
+	///
+	/// Initially, no port is selected (`selected_port` is `None`).
+	/// The `generate` method must be called to select a port.
+	///
+	pub fn new() -> Self {
+		PortGenerator {
+			selected_port: None,
+		}
+	}
 
-    /// Generates and selects an available port.
-    ///
-    /// This method requests the operating system to provide an available ephemeral port
-    /// by binding a `TcpListener` to port `0`. The OS assigns a free port, which is then
-    /// retrieved and stored in `selected_port`.
-    ///
-    /// # Returns
-    ///
-    /// Returns a mutable reference to itself, allowing for method chaining.
+	/// Generates and selects an available port.
+	///
+	/// This method requests the operating system to provide an available ephemeral port
+	/// by binding a `TcpListener` to port `0`. The OS assigns a free port, which is then
+	/// retrieved and stored in `selected_port`.
+	///
+	/// # Returns
+	///
+	/// Returns a mutable reference to itself, allowing for method chaining.
+	pub fn generate(&mut self) -> &mut Self {
+		if let Ok(listener) = TcpListener::bind(("127.0.0.1", 0)) {
+			if let Ok(addr) = listener.local_addr() {
+				self.selected_port = Some(addr.port());
+			}
+		}
+		self
+	}
 
-    pub fn generate(&mut self) -> &mut Self {
-        if let Ok(listener) = TcpListener::bind(("127.0.0.1", 0)) {
-            if let Ok(addr) = listener.local_addr() {
-                self.selected_port = Some(addr.port());
-            }
-        }
-
-        self
-    }
-
-    /// Retrieves the selected port number.
-    ///
-    /// This method returns the port number selected by the `generate` method.
-    /// If `generate` has not been called or was unsuccessful, this will return `None`.
-    ///
-    /// # Returns
-    ///
-    /// An `Option<u16>` representing the selected port.
-    ///
-
-    pub fn selected_port(&self) -> Option<u16> {
-        self.selected_port
-    }
+	/// Returns the selected port, if available.
+	pub fn selected_port(&self) -> Option<u16> {
+		self.selected_port
+	}
 }
